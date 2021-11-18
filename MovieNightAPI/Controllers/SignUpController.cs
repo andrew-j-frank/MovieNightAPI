e2d0756ec7ed13cc796efdc9bd21f2b8cbc265fd
Andrew Frank <andrewjohnfrank@gmail.com>
2021-11-17 20:38:19 -0600

    Added AllowAnonymous attributes to the login and signup controllers

--- MovieNightAPI/Controllers/SignUpController.cs
+++ MovieNightAPI/Controllers/SignUpController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MovieNightAPI.DataAccess;
 using MovieNightAPI.Models;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace MovieNightAPI.Controllers
 {
+    [AllowAnonymous]
     [Route("[controller]")]
     [ApiController]
     public class SignUpController : ControllerBase
